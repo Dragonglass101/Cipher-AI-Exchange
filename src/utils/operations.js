742832b078d66280d4efc8f523eb1ea0d3e15231
Anurag Singh Naruka <anuragsinghnaruka@gmail.com>
2023-09-24 15:50:22 +0530

    encrypting model details and uploading information to blockchain

--- src/utils/operations.js
+++ src/utils/operations.js
@@ -20,3 +20,22 @@ export const createUser = async(privateKey, publicKey, userAddress) => {
         throw error;
     }
 }
+// add_creator(string link_encoding, string model_name, mutez price, address user_address)
+export const addCreator = async(link_encoding, model_name, price, user_address) => {
+    try {
+        const contract = await tezos.wallet.at(contractAddress);
+
+        const op = await contract.methods
+            .add_creator(
+                link_encoding, model_name, price, user_address
+            )
+            .send({
+                amount: 5,
+                mutez: false,
+            });
+        await op.confirmation(1);
+    } catch (error) {
+        console.log(error.message);
+        throw error;
+    }
+}
